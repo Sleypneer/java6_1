73dbdcf4c340210658b58d2f0b37b334a48f7793
sleypneer <sleypneer@mail.ru>
2020-10-01 18:00:21 +0300

    add project

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,69 @@
+package ru.netology.stats;
+
+
+public class StatsService {
+
+    public long findSum(long[] values) {
+        long proseeds = 0;
+        for(int i=0; i<values.length; i++) {
+            proseeds=proseeds+values[i];
+        }
+        return proseeds;
+    }
+
+    public long findAvgSales(long[] values) {
+        long proseeds = 0;
+        long avgsales;
+        for(int i=0; i<values.length; i++) {
+            proseeds=proseeds+values[i];
+        }
+        avgsales = proseeds / values.length;
+
+        return avgsales;
+    }
+
+    public long findMaxMonth(long[] values) {
+            long maxMonth = 0;
+            long currentValue = values[0];
+            for (int i = 0; i < values.length; i++) {
+                if (currentValue <= values[i]) {
+                    maxMonth = i + 1;
+                    currentValue = values[i];
+                }
+            }
+        return maxMonth;
+    }
+
+
+    public long findMinMonth(long[] values) {
+        long minMonth = 0;
+        long currentValue = values[0];
+        for (int i = 0; i < values.length; i++) {
+            if (currentValue >= values[i]) {
+                minMonth = i +1;
+                currentValue = values[i];
+            }
+        }
+        return minMonth;
+    }
+
+    public long findUnderAvgSales(long[] values) {
+        long listUASales = 0;
+        long avgSales = findAvgSales(values);
+        for (long currentValue : values) {
+            if (currentValue < avgSales) listUASales++;
+        }
+        return listUASales;
+    }
+
+    public long findUpAvgSum(long[] values) {
+        long listUpASales = 0;
+        long avgSales = findAvgSales(values);
+        for (long currentValue : values) {
+            if (currentValue > avgSales) listUpASales++;
+        }
+        return listUpASales;
+    }
+
+
+}
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,83 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StatsServiceTest {
+
+    long[] values = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+    @Test
+
+    void shoudFindProseeds() {
+
+        StatsService service = new StatsService();
+
+        long proseeds = service.findSum(values);
+        long expected = 180;
+
+        assertEquals(expected, proseeds);
+        System.out.println ("Сумма всех продаж =" + proseeds);
+    }
+
+    @Test
+
+    void shoudFindAverageSales() {
+
+        StatsService service = new StatsService();
+
+        long avgsales = service.findAvgSales(values);
+        long expected = 15;
+
+        assertEquals(expected, avgsales);
+        System.out.println ("Средняя сумма продаж =" + avgsales);
+    }
+
+    @Test
+
+    void shoudFindMaxMonth() {
+
+        StatsService service = new StatsService();
+
+        long maxMonth = service.findMaxMonth(values);
+        long expected = 8;
+
+        assertEquals(expected, maxMonth);
+        System.out.println ("Месяц с пиком продаж =" + maxMonth);
+    }
+
+    @Test
+    void shouldFindMinMonth() {
+        StatsService service = new StatsService();
+        long minMonth = service.findMinMonth(values);
+        long expected = 9;
+
+        assertEquals(expected, minMonth);
+        System.out.println ("Месяц минимальных продаж =" + minMonth);
+    }
+
+    @Test
+    void shouldFindUnderAvgSum() {
+        StatsService service = new StatsService();
+        long listUASales = service.findUnderAvgSales(values);
+        long expected = 5;
+
+        assertEquals(expected, listUASales);
+        System.out.println ("Месяцев с продажами ниже среднего =" + listUASales);
+    }
+
+    @Test
+    void shouldFindUpAvgSum() {
+        StatsService service = new StatsService();
+        long listUpASales = service.findUpAvgSum(values);
+        long expected = 5;
+
+        assertEquals(expected, listUpASales);
+        System.out.println ("Месяцев с продажами выше среднего =" + listUpASales);
+    }
+
+
+
+
+}
